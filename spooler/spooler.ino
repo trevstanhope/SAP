3ff093df2c2ebbbe8d1d15859517c44af21f6916
Trevor Stanhope <tpstanhope@gmail.com>
2014-06-14 14:02:51 -0400

    new spooler file ala bharath

--- spooler/spooler.ino
+++ spooler/spooler.ino
@@ -1,69 +1,121 @@
-/* 
-  Spooler
-*/
-
-/* --- Headers --- */
-#include <SoftwareSerial.h>
+#include <AFMotor.h>
 #include <Servo.h>
-#define SERVO_LEFT_PIN 9
-#define SERVO_RIGHT_PIN 10
-
-/* --- Actions ---*/
-const char MOVE_FORWARD = 'F';
-const char MOVE_BACKWARD = 'B';
-const char TURN_LEFT = 'L';
-const char TURN_RIGHT = 'R';
-
-/* --- Errors --- */
-const char ERROR_NONE = 0;
-const char ERROR_ACTION = 9;
 
 /* --- Constants --- */
-const int TIME_WAIT = 1000;
-const int CR_SERVO_CCW = 50;
-const int CR_SERVO_CW = 150;
-const int CR_SERVO_STOP = 100;
-const int BAUD = 9600;
+const int LEFT_TREAD_MOTOR = 1;
+const int RIGHT_TREAD_MOTOR = 2;
+const int BAUD_RATE = 9600;
+const int MOTOR_SPEED = 200;
+const char MOVE_FORWARD = '1';
+const char MOVE_BACKWARD = '2';
+const char TURN_LEFT = '3';
+const char TURN_RIGHT = '4';
+const char BOOM_RIGHT = '5';
+const char BOOM_LEFT = '6';
+const char BOOM_CENTER = '7';
+const int TURN_DELAY = 1000;
+const int MOVE_DELAY = 500;
+const int BOOM_PIN = 10;
+const int DEGREES_RIGHT=141;
+const int DEGREES_LEFT=25;
+const int DEGREES_CENTER=83;
+const int BOOM_DELAY = 100;
 
-/* --- Variables --- */
-char action;
-char error;
-Servo servo_left;
-Servo servo_right;
+/* --- Objects --- */
+AF_DCMotor left_tread(LEFT_TREAD_MOTOR);
+AF_DCMotor right_tread(RIGHT_TREAD_MOTOR);
+Servo boom;
+int COMMAND;
 
 /* --- Setup --- */
 void setup() {
-  // Left
-  servo_left.attach(SERVO_RIGHT_PIN);
-  servo_left.write(CR_SERVO_STOP);
-  // Right
-  servo_right.attach(SERVO_LEFT_PIN);
-  servo_right.write(CR_SERVO_STOP);
-  // Serial
-  Serial.begin(BAUD);
+  Serial.begin(BAUD_RATE);
+  left_tread.setSpeed(MOTOR_SPEED);
+  right_tread.setSpeed(MOTOR_SPEED);
+  left_tread.run(RELEASE); // initialize motor off
+  right_tread.run(RELEASE); // initialize motor off
+  boom.attach(BOOM_PIN);
 }
 
-/* --- Loop --- */
 void loop() {
-  action = Serial.read();
-  switch(action) {
+  
+  // Check for incoming byte from USB
+  COMMAND = Serial.read();
+  
+  // Execute command
+  switch (COMMAND) {
     case MOVE_FORWARD:
+      move_forward();
       break;
     case MOVE_BACKWARD:
-      break;
-    case TURN_LEFT:
+      move_backward();
       break;
     case TURN_RIGHT:
+      turn_right();
       break;
+    case TURN_LEFT:
+      turn_left();
+      break;
+    case BOOM_RIGHT:
+      boom_right();
+      break;
+    case BOOM_LEFT:
+      boom_left();
+      break;  
+    case BOOM_CENTER:
+      boom_center();
+      break;   
     default:
       break;
   }
-  if (error != ERROR_ACTION) {
-    Serial.flush();
-    delay(TIME_WAIT);
-    Serial.println(error);
-  }
-  else {
-    Serial.flush();
-  }
 }
+
+/* --- Tread Functions --- */
+void move_forward() {
+  left_tread.run(FORWARD);
+  right_tread.run(BACKWARD);
+  delay(MOVE_DELAY);
+  left_tread.run(RELEASE); // initialize motor off
+  right_tread.run(RELEASE); // initialize motor off
+}
+
+void move_backward() {
+  left_tread.run(BACKWARD);
+  right_tread.run(FORWARD);
+  delay(MOVE_DELAY);
+  left_tread.run(RELEASE); // initialize motor off
+  right_tread.run(RELEASE); // initialize motor off
+}
+
+void turn_right() {
+  left_tread.run(BACKWARD);
+  right_tread.run(BACKWARD);
+  delay(TURN_DELAY);
+  left_tread.run(RELEASE); // initialize motor off
+  right_tread.run(RELEASE); // initialize motor off
+}
+
+void turn_left() {
+  left_tread.run(FORWARD);
+  right_tread.run(FORWARD);
+  delay(TURN_DELAY);
+  left_tread.run(RELEASE); // initialize motor off
+  right_tread.run(RELEASE); // initialize motor off
+}
+
+/* --- Boom --- */
+void boom_right() {
+  boom.write(DEGREES_RIGHT);
+  delay(BOOM_DELAY);
+}
+
+void boom_left() {
+  boom.write(DEGREES_LEFT);
+  delay(BOOM_DELAY);  
+}
+
+void boom_center() {
+  boom.write(DEGREES_CENTER);
+  delay(BOOM_DELAY);  
+}
+
