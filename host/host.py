bc6494b347f67fc5995fc744bfaba13a639fa82b
Trevor Stanhope <tpstanhope@gmail.com>
2014-06-18 05:15:54 -0400

    big fixes, mostly working

--- host/host.py
+++ host/host.py
@@ -70,12 +70,19 @@ class Host:
             trees = self.state_machine.predict_trees()
             robots = self.state_machine.predict_robots()
             path = self.state_machine.path
-            self.display.show_board(trees, robots, path)
-    
+            true_path = self.state_machine.true_path
+            self.display.show_board(trees, robots, path, true_path)
+            self.database.store(request, response)
+
     ## Stop 
     def stop(self, object):
         print('[Stopping] %s' % datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S"))
         self.running = False
+        
+    ## Reset 
+    def reset(self, object):
+        print('[Stopping] %s' % datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S"))
+        self.state_machine = StateMachine(self)
         
 # ZMQ Server Object
 import zmq
@@ -138,16 +145,17 @@ class Database(object):
         except Exception as error:
             print('\tERROR: %s' % str(error))
             
-    ## Store to Mongo(DICT) --> _ID
+    ## Store to Mongo #!TODO
     def store(self, request, response):
         print('[Storing to Database]  %s' % datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S"))
         try:
             doc = {
-                'request': request,
-                'response':response
+                'time' : datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S"),
+                'request' : request,
+                'response': response
             }
             doc_id = self.collection.insert(doc)
-            print doc_id
+            print('\tDoc ID: %s' % str(doc_id))
             return doc_id
         except Exception as error:
             print('\tERROR: %s' % str(error))
@@ -181,6 +189,11 @@ class Display(object):
             self.button_stop.connect("clicked", object.stop)
             self.hbox.pack_start(self.button_stop, True, True, 0)
             self.button_stop.show()
+            ### Reset Button
+            self.button_reset = gtk.Button("Reset")
+            self.button_reset.connect("clicked", object.reset)
+            self.hbox.pack_start(self.button_reset, True, True, 0)
+            self.button_reset.show()
             print('\tOKAY')
         except Exception as error:
             print('\tERROR: %s' % str(error))
@@ -199,7 +212,7 @@ class Display(object):
             print('\tERROR: %s' % str(error))
     
     ## Show Board State
-    def show_board(self, tree_locations, robot_locations, path):
+    def show_board(self, tree_locations, robot_locations, path, true_path):
         print('[Displaying Board] %s' % datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S"))
         try:
             board = cv2.imread('board.jpg')
@@ -223,6 +236,13 @@ class Display(object):
                 (x2, y2) = path[i+1]
                 pt2 = (x2 * w / 96, y2 * h / 96)
                 cv2.line(board, pt1, pt2, (128,128,128), thickness=4, lineType=8, shift=0)
+            ### Draw True Path
+            for i in range(len(true_path) - 1):
+                (x1, y1) = true_path[i]
+                pt1 = (x1 * w / 96, y1 * h / 96)
+                (x2, y2) = true_path[i+1]
+                pt2 = (x2 * w / 96, y2 * h / 96)
+                cv2.line(board, pt1, pt2, (0,128,255), thickness=4, lineType=8, shift=0)
             cv2.imshow('BOARD', board)
             if cv2.waitKey(5) == 27:
                 pass
@@ -240,8 +260,16 @@ class StateMachine(object):
         try:
             self.map = numpy.zeros([object.X_SIZE, object.Y_SIZE])
             self.NUM_TREES = object.NUM_TREES
+            self.PIVOT_DISTANCE = object.PIVOT_DISTANCE
+            self.X_SIZE = object.X_SIZE
+            self.Y_SIZE = object.Y_SIZE
+            self.DIST_MIN = object.DIST_MIN
+            self.DIST_MAX = object.DIST_MAX
+            self.CAMERA_ERROR = object.CAMERA_ERROR
+            self.ORIENTATION_PRECISION = object.ORIENTATION_PRECISION
             self.detected_trees = []
             self.path = []
+            self.true_path = []
             self.snapshots = {}
             self.tree_positions = [
                 (16, 32),
@@ -265,11 +293,13 @@ class StateMachine(object):
                 (80, 64),
                 (80, 80),
             ]
-            self.robots = {
-                'spooler' : (48, 0, numpy.pi/2.0),
-                'scout_left' : (8, 0, numpy.pi/2.0),
-                'scout_right' : (88, 0, numpy.pi/2.0)
-            }
+            self.spooler_position = (object.SPOOLER_DEFAULT_X, object.SPOOLER_DEFAULT_Y, object.SPOOLER_DEFAULT_T)
+            self.scout_left_position = (object.SCOUT_LEFT_DEFAULT_X, object.SCOUT_LEFT_DEFAULT_Y, object.SCOUT_LEFT_DEFAULT_T)
+            self.scout_right_position = (object.SCOUT_RIGHT_DEFAULT_X, object.SCOUT_RIGHT_DEFAULT_Y, object.SCOUT_RIGHT_DEFAULT_T)
+            self.SPOOLER_LARGE_MOVEMENT = object.SPOOLER_LARGE_MOVEMENT
+            self.SPOOLER_FINE_MOVEMENT = object.SPOOLER_FINE_MOVEMENT
+            self.SPOOLER_LARGE_TURN = object.SPOOLER_LARGE_TURN
+            self.SPOOLER_FINE_TURN = object.SPOOLER_FINE_TURN
             print('\tOKAY')
         except Exception as error:
             print('\tERROR: %s' % str(error))
@@ -291,20 +321,80 @@ class StateMachine(object):
         print('[Handling Spooler Request] %s' % datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S"))
         try:
             snapshot = request['snapshot']
-            position = self.robots[request['id']]
+            position = self.spooler_position
             self.detected_trees = self.predict_trees()
             if len(self.detected_trees) < self.NUM_TREES:
                 print('\tWARNING: Not enough trees detected')
                 self.update_map(snapshot, position) 
+                command = 'wait'
             elif len(self.path) < self.NUM_TREES:
                 print('\tWARNING: Ready to calculate path')
                 self.path = self.find_path(self.detected_trees)
-            else:
+                self.true_path = self.find_true_path(self.path)
+                self.target_points = self.true_path[1:]
+                command = 'wait'
+            elif len(self.target_points) > 0:
                 print('\tWARNING: Running Path')
-                #!TODO decide movement actions for spooler 
+                #!TODO decide movement actions for spooler
+                (target_x, target_y) = self.target_points[0]
+                (x, y, t) = position
+                if numpy.allclose(x, target_x, atol=1) and numpy.allclose(y, target_y, atol=1):
+                    print('\tWARNING: Reached Target, proceeding to Next Point!')
+                    self.target_points.pop(0)
+                    (target_x, target_y) = self.target_points[0]
+                print('\tTarget Point: %s' % str((target_x, target_y)))
+                target_orientation = self.orientation((x,y), (target_x, target_y))
+                print('\tTarget Orientation: %s' % str(target_orientation))
+                distance_to_target = int(numpy.linalg.norm(numpy.array((target_x,target_y)) - numpy.array((x,y))))
+                print('\tDistance to Target: %s' % str(distance_to_target))
+                print('\tCurrent Orientation: %s' % str(t))
+                print('\tCurrent Position: %s' % str((x,y)))
+                ### Move Forward
+                if numpy.allclose(t, target_orientation, atol=self.SPOOLER_FINE_TURN):
+                    print('\tCosine: %s' % str(numpy.cos(t)))
+                    print('\tSin: %s' % (numpy.sin(t)))
+                    if numpy.less(distance_to_target, self.SPOOLER_LARGE_MOVEMENT):
+                        command = 'forward_fine'
+                        new_x = int(x + self.SPOOLER_FINE_MOVEMENT * numpy.cos(t))
+                        new_y = int(y + self.SPOOLER_FINE_MOVEMENT * numpy.sin(t))
+                    else:
+                        command = 'forward_large'
+                        new_x = int(x + self.SPOOLER_LARGE_MOVEMENT * numpy.cos(t))
+                        new_y = int(y + self.SPOOLER_LARGE_MOVEMENT * numpy.sin(t))
+                    self.spooler_position = (new_x, new_y, t)
+                ### Turn Right #!TODO Fine vs Large, Turning right from wide to 0 also broken
+                elif numpy.less(t, target_orientation) and ((target_orientation-t) < numpy.pi):
+                    command = 'right'
+                    new_t = t + self.SPOOLER_FINE_TURN
+                    if new_t > 2 * numpy.pi:
+                        new_t_round = numpy.around(new_t - 2 * numpy.pi, self.ORIENTATION_PRECISION)
+                    else:
+                        new_t_round = numpy.around(new_t, self.ORIENTATION_PRECISION)
+                    self.spooler_position = (x, y, new_t_round)
+                ### Turn Right #!TODO fine vs large
+                elif numpy.greater(t, target_orientation) or ((target_orientation - t) < 2 * numpy.pi):
+                    command = 'left'
+                    new_t = t - self.SPOOLER_FINE_TURN
+                    if new_t < 0:
+                        new_t = numpy.around(2 * numpy.pi + new_t, self.ORIENTATION_PRECISION)
+                    else:
+                        new_t = numpy.around(new_t, self.ORIENTATION_PRECISION)
+                    self.spooler_position = (x, y, new_t)
+                ### Move Backward #!TODO
+                elif False:
+                    command = 'backward'
+                else:
+                    command = 'wait'
+                time.sleep(1)
+            else:
+                command = 'wait'
         except Exception as error:
             print('\tERROR: %s' % str(error))
-        response = request
+        print('\tCommand: %s' % str(command))
+        response = {
+            'type' : 'response', 
+            'command' : command
+        }
         return response
     
     ##!TODO Handle Scout (Left)
@@ -328,28 +418,15 @@ class StateMachine(object):
     ## Update Map   
     def update_map(self, snapshot, position):
         print('[Updating Map] %s' % datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S"))
-#        try:
-#            position_as_string = str(list(position))
-#            previous = self.snapshots[position_as_string]
-#            print('\tWARNING: Snapshot already taken at this position!')
-#            return None # do not add redundant snapshots to tree map
-#        except Exception as error:
-#            print('\tERROR: %s' % str(error))
-#            self.snapshots[position_as_string] = snapshot
         (x, y, t) = position
-        error = 0.1
-        DIST_MIN = 10
-        DIST_MAX = 106
-        X_MAX = 96
-        Y_MAX = 96
         for offset in snapshot:
-            for orientation in [-error, 0, error]: # more then 3
-                x1 = x + numpy.cos(t + offset + error) * DIST_MIN
-                y1 = y + numpy.sin(t + offset + error) * DIST_MIN
-                for dist in range(DIST_MIN, DIST_MAX):
-                    x2 = x1 + numpy.cos(t + offset + error) * dist
-                    y2 = y1 + numpy.sin(t + offset + error) * dist
-                    if (x2 >= X_MAX - 2) or (y2 >= Y_MAX - 2):
+            for orientation in [-self.CAMERA_ERROR, 0, self.CAMERA_ERROR]: # #!TODO more then 3
+                x1 = x + numpy.cos(t + offset + self.CAMERA_ERROR) * self.DIST_MIN
+                y1 = y + numpy.sin(t + offset + self.CAMERA_ERROR) * self.DIST_MIN
+                for dist in range(self.DIST_MIN, self.DIST_MAX):
+                    x2 = x1 + numpy.cos(t + offset + self.CAMERA_ERROR) * dist
+                    y2 = y1 + numpy.sin(t + offset + self.CAMERA_ERROR) * dist
+                    if (x2 >= self.X_SIZE - 2) or (y2 >= self.Y_SIZE - 2):
                         break
                     elif (x2 <= 0) or (y2 <= 0):
                         break
@@ -375,11 +452,16 @@ class StateMachine(object):
     ## Predict Robot Locations
     def predict_robots(self):
         print('[Predicting Robot Locations] %s' % datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S"))
-        return self.robots
+        robot_positions = {
+            'scout_left' : self.scout_left_position,
+            'scout_right' : self.scout_right_position,
+            'spooler' : self.spooler_position
+        }
+        return robot_positions
     
     ## Calculate Minimum Hamiltonian Path
     def find_path(self, trees):
-        print('[Finding Shortest Path] %s' % datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S"))
+        print('[Find Shortest Path] %s' % datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S"))
         points = trees.append((48,1)) # add start
         paths = [ p for p in it.permutations(trees) ]
         path_distances = [ sum(map(lambda x: self.dist(x[0],x[1]),zip(p[:-1],p[1:]))) for p in paths ]
@@ -388,9 +470,156 @@ class StateMachine(object):
         print('\tPath: %s' % str(shortest_path[::-1]))
         return shortest_path[::-1]
     
+    ## Calculate the True Path for Spooler to Travel
+    def find_true_path(self, path):
+        print('[Find True Path] %s' % datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S"))
+        true_path = [(48,1)]
+        ## Add pivots
+        for i in range(1, len(path) - 1):
+            try:
+                previous = path[i-1]
+                current = path[i]
+                next = path[i+1]
+                point = self.pivot_point(previous, current, next)
+                true_path.append(point)
+            except Exception as error:
+                print('\tERROR: %s' % str(error))
+        ## Add anchor points
+        current = path[-2]
+        last = path[-1]
+        anchor_points = self.anchor_points(current, last)
+        for point in anchor_points:
+            true_path.append(point)
+        print('\tTrue Path: %s' % str(true_path))
+        return true_path
+    
     ## Distance Function
     def dist(self, x, y):
-        return math.hypot(y[0]-x[0],y[1]-x[1])
+        return math.hypot( y[0] - x[0], y[1]-x[1] )
+        
+    ## Pivot Point #!TODO fix bad pivots
+    def pivot_point(self, tree1, tree2, tree3):
+        print('\t[Calculating Pivot Point]')
+        (x1, y1) = tree1
+        (x2, y2) = tree2
+        (x3, y3) = tree3
+        m1 = self.slope(tree1, tree3)
+        m2 = self.perpendicular_slope(m1)
+        print('\t\tSlope 1: %s' % str(m1))
+        print('\t\tSlope 2: %s' % str(m2))
+        ## Conditional for direction
+        if (x2 < x1) or (x2 < x3):
+            dx = -self.PIVOT_DISTANCE
+        else:
+            dx = self.PIVOT_DISTANCE
+        if (y2 > y1) and (y2 > y3):
+            dy = self.PIVOT_DISTANCE
+        else:
+            dy = -self.PIVOT_DISTANCE
+        ## Handle Infinite cases
+        if numpy.isinf(m2):
+            x4 = x2 + dx
+            y4 = y2 + dy
+        else:
+            x4 = x2 + dx / numpy.sqrt(1 + m2**2)
+            y4 = y2 + dy * m2 / numpy.sqrt(1 + m2**2)
+        print('\t\tdx: %s' % str(dx))
+        print('\t\tdy: %s' % str(dx))
+        point = (int(x4), int(y4))
+        return point
+    
+    ## Anchor Points #!TODO improve point calculations
+    def anchor_points(self, tree1, tree2):
+        print ('\t[Calculating Anchor Points]')
+        print('\t\tSecond-to-Last Tree: %s' % str(tree1))
+        print('\t\tLast Tree: %s' % str(tree2))
+        (x1, y1) = tree1
+        (x2, y2) = tree2
+        m1 = self.slope(tree1, tree2)
+        m2 = self.perpendicular_slope(m1)
+        if numpy.isinf(m1):
+            x3 = int(x2)
+            y3 = int(y2 + self.PIVOT_DISTANCE)
+            x5 = int(x2)
+            y5 = int(y2 - self.PIVOT_DISTANCE)
+        else:
+            x3 = int(x2 + self.PIVOT_DISTANCE / numpy.sqrt(1 + m1**2))
+            y3 = int(y2 + self.PIVOT_DISTANCE * m1 / numpy.sqrt(1 + m1**2))
+            x5 = int(x2 - self.PIVOT_DISTANCE / numpy.sqrt(1 + m1**2))
+            y5 = int(y2 - self.PIVOT_DISTANCE * m1 / numpy.sqrt(1 + m1**2))
+        if numpy.isinf(m2):
+            x4 = int(x2)
+            y4 = int(y2 + self.PIVOT_DISTANCE)
+            x6 = int(x2)
+            y6 = int(y2 - self.PIVOT_DISTANCE)
+        else:
+            x4 = int(x2 + self.PIVOT_DISTANCE / numpy.sqrt(1 + m2**2))
+            y4 = int(y2 + self.PIVOT_DISTANCE * m2 / numpy.sqrt(1 + m2**2))
+            x6 = int(x2 - self.PIVOT_DISTANCE / numpy.sqrt(1 + m2**2))
+            y6 = int(y2 - self.PIVOT_DISTANCE * m2 / numpy.sqrt(1 + m2**2))
+        points = [ (x3, y3), (x4, y4), (x5, y5), (x6, y6) ]
+        return points
+        
+    ## Find slope
+    def slope(self, pt1, pt2):
+        (x1, y1) = pt1
+        (x2, y2) = pt2
+        try:
+            m = float(y2 - y1) / float(x2 - x1)
+        except Exception as error:
+            if (x2 - x1) >= 0:
+                m = numpy.inf
+            elif (x2 - x1) < 0:
+                m = -numpy.inf
+        return m
+    
+    ## Find Perpedicular Slope
+    def perpendicular_slope(self, m1):
+        try:
+            m2 = -1 / m1
+        except Exception as error:
+            m2 = numpy.inf
+        return m2
+    
+    ## Angle of Orientation
+    def orientation(self, pt1, pt2):
+        (x, y) = pt1
+        (target_x, target_y) = pt2
+        try:
+            m = float(target_y - y) / float(target_x - x)
+        except Exception as error:
+            print('\tERROR: %s' % str(error))
+            if (target_y - y) > 0:
+                m = numpy.inf
+            elif (target_y - y) < 0:
+                m = -numpy.inf
+        at = numpy.arctan(m)
+        if target_y > y:
+            if target_x > x:
+                t = at
+            elif target_x < x:
+                t = at + numpy.pi
+            else:
+                t = numpy.pi / 2.0
+        elif target_y < y:
+            if target_x > x:
+                t = at + 2.0 * numpy.pi
+            elif target_x < x:
+                t = at + numpy.pi
+            else:
+                t = 3.0 * numpy.pi / 2.0
+        else:
+            if target_x > x:
+                t = 0
+            elif target_x < x:
+                t = numpy.pi
+            else:
+                t = numpy.nan # a = b
+        print('\tArcTan: %s' % str(at))
+        print('\tAdjusted ArcTan: %s' % str(t))
+        print('\tSlope: %s' % str(m))
+        t_round = numpy.around(t, self.ORIENTATION_PRECISION)
+        return t_round
     
     ## Close State Machine
     def close(self):
